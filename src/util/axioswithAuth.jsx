21aa41d500ee40822f7eb557bd43c8d4ec0b0e17
CalebAllard <allard.caleb@gmail.com>
2020-04-04 16:32:58 -0400

    deploying

--- src/util/axioswithAuth.jsx
+++ src/util/axioswithAuth.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-        baseURL: 'http://localhost:8080',
+        baseURL: 'https://bitadly.herokuapp.com/',
         headers: {
             Authorization: token
         }
